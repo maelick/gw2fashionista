debe22f6e276d870d85e18136be74abcd3433e8a
Maëlick Claes <himself@maelick.net>
2026-06-02 13:01:23 +0000

    add parse tests

---
+++ rust/gw2fashionista-core/tests/parse_chatlink.rs
@@ -0,0 +1,109 @@
+#[cfg(test)]
+mod tests {
+    use gw2fashionista_core::domain::{chatlink::ChatLink, error::ChatLinkError};
+    use std::assert_matches;
+
+    const EMPTY_TEMPLATE: &str = "[&DwAAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAAAAAAAAAAAAAD/fw==]";
+    const ZIZI_TEMPLATE: &str = "D1sDPQkBAAEAAQABAAwAGAURBhEGAQAjABgFEQYBAAEA/AABABEGGAUYBdIDGAURBgEAAQALAAEAEQYRBgEAohYYBREGAQABAHwAAQABAAEAAQDjE6APPBI8Ej0SAAD+fg==";
+    const ZIZI_ARMOR_TEMPLATE: &str = "[&DwAAPQkBAAEAAQABAAwAGAURBhEGAQAjABgFEQYBAAEA/AABABEGGAUYBdIDGAURBgEAAQALAAEAEQYRBgEAohYYBREGAQABAAAAAQABAAEAAQAAAAAAAAAAAAAAAAD/fw==]";
+    
+    #[test]
+    fn test_parse_empty_link() {
+        let raw = "";
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Err(ChatLinkError::EmptyPayload));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Err(ChatLinkError::EmptyPayload));
+    }
+
+    #[test]
+    fn test_parse_not_chat_link() {
+        let raw = "This is not a chat link";
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Err(ChatLinkError::InvalidString));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Err(ChatLinkError::InvalidString));
+    }
+
+    #[test]
+    fn test_parse_invalid_base64() {
+        let raw = "hello";
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Err(ChatLinkError::InvalidBase64(_)));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Err(ChatLinkError::InvalidBase64(_)));
+    }
+
+    #[test]
+    fn test_parse_invalid_link_type() {
+        let raw = "abcd";
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Err(ChatLinkError::UnknownType(_)));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Err(ChatLinkError::UnknownType(_)));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_parse_invalid_length() {
+        let raw = "DwAAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAAAAAAAAAD/fw==";
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Err(ChatLinkError::TruncatedData(_)));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Err(ChatLinkError::TruncatedData(_)));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_parse_empty() {
+        let raw = EMPTY_TEMPLATE;
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Ok(_));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Ok(_));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_parse_zizi() {
+        let raw = ZIZI_TEMPLATE;
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Ok(_));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Ok(_));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_parse_zizi_armor_only() {
+        let raw = ZIZI_ARMOR_TEMPLATE;
+        
+        let result = ChatLink::try_from(raw);
+        assert_matches!(result, Ok(_));
+
+        let raw_with_brackets = format!("[&{}]", raw);
+        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
+        assert_matches!(result_with_brackets, Ok(_));
+    }
+}
