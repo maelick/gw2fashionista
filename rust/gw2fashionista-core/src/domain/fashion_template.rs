50498378f5ddad22f5e08d972e7bf7b2bd0adea4
Maëlick Claes <himself@maelick.net>
2026-06-03 10:28:12 +0000

    add from trait implementations for dyes and skins ids

--- rust/gw2fashionista-core/src/domain/fashion_template.rs
+++ rust/gw2fashionista-core/src/domain/fashion_template.rs
@@ -75,8 +75,24 @@ impl From<FashionTemplate> for Vec<u8> {
 pub struct SkinId(u16);
 
 impl SkinId {
+    pub fn new(id: u16) -> Self {
+        SkinId(id)
+    }
+
     pub fn from_cursor(cursor: &mut Cursor<&[u8]>) -> Result<Self, std::io::Error> {
         Ok(SkinId(cursor.read_u16::<LittleEndian>()?))
+    }
+}
+
+impl From<u16> for SkinId {
+    fn from(id: u16) -> Self {
+        Self::new(id)
+    }
+}
+
+impl From<SkinId> for u16 {
+    fn from(SkinId(id): SkinId) -> u16 {
+        id
     }
 }
 
@@ -84,8 +100,24 @@ impl SkinId {
 pub struct DyeId(u16);
 
 impl DyeId {
+    pub fn new(id: u16) -> Self {
+        DyeId(id)
+    }
+
     pub fn from_cursor(cursor: &mut Cursor<&[u8]>) -> Result<Self, std::io::Error> {
         Ok(DyeId(cursor.read_u16::<LittleEndian>()?))
+    }
+}
+
+impl From<u16> for DyeId {
+    fn from(id: u16) -> Self {
+        Self::new(id)
+    }
+}
+
+impl From<DyeId> for u16 {
+    fn from(DyeId(id): DyeId) -> u16 {
+        id
     }
 }
 
@@ -93,6 +125,10 @@ impl DyeId {
 pub struct Dyes(DyeId, DyeId, DyeId, DyeId);
 
 impl Dyes {
+    pub fn new(dye1: DyeId, dye2: DyeId, dye3: DyeId, dye4: DyeId) -> Self {
+        Dyes(dye1, dye2, dye3, dye4)
+    }
+
     pub fn from_cursor(cursor: &mut Cursor<&[u8]>) -> Result<Self, std::io::Error> {
         Ok(Dyes(
             DyeId::from_cursor(cursor)?,
@@ -100,6 +136,30 @@ impl Dyes {
             DyeId::from_cursor(cursor)?,
             DyeId::from_cursor(cursor)?,
         ))
+    }
+}
+
+impl From<(u16, u16, u16, u16)> for Dyes {
+    fn from((id1, id2, id3, id4): (u16, u16, u16, u16)) -> Self {
+        Self::new(id1.into(), id2.into(), id3.into(), id4.into())
+    }
+}
+
+impl From<(DyeId, DyeId, DyeId, DyeId)> for Dyes {
+    fn from((dye1, dye2, dye3, dye4): (DyeId, DyeId, DyeId, DyeId)) -> Self {
+        Self::new(dye1, dye2, dye3, dye4)
+    }
+}
+
+impl From<Dyes> for (DyeId, DyeId, DyeId, DyeId) {
+    fn from(Dyes(dye1, dye2, dye3, dye4): Dyes) -> (DyeId, DyeId, DyeId, DyeId) {
+        (dye1, dye2, dye3, dye4)
+    }
+}
+
+impl From<Dyes> for (u16, u16, u16, u16) {
+    fn from(Dyes(dye1, dye2, dye3, dye4): Dyes) -> (u16, u16, u16, u16) {
+        (dye1.into(), dye2.into(), dye3.into(), dye4.into())
     }
 }
 
