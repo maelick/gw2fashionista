961f21ff75e2272d3e1b512a8331974d12789d45
Maëlick Claes <himself@maelick.net>
2026-06-03 10:30:30 +0000

    implement assertions for happy path tests

--- rust/gw2fashionista-core/tests/parse_chatlink.rs
+++ rust/gw2fashionista-core/tests/parse_chatlink.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
-    use gw2fashionista_core::domain::{chatlink::ChatLink, error::ChatLinkError};
+    use gw2fashionista_core::domain::{chatlink::ChatLink, error::ChatLinkError, fashion_template::{DyableSkin, EquipmentSlot, FashionTemplate, Skin}, skin_type::SkinType};
+use strum::IntoEnumIterator;
     use std::assert_matches;
 
     const EMPTY_TEMPLATE: &str = "DwAAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAAAAAAAAAAAAAD/fw==";
@@ -70,36 +71,122 @@ mod tests {
     #[test]
     fn test_parse_empty() {
         let raw = EMPTY_TEMPLATE;
+        let expected_slots = SkinType::iter().map(|skin_type| {
+            (skin_type, empty_skin(skin_type))
+        }).collect();
+        let expected_template = FashionTemplate::new(expected_slots);
         
         let result = ChatLink::try_from(raw);
-        assert_matches!(result, Ok(_));
+        assert_matches!(result, Ok(ChatLink::WardrobeTemplate(actual)) if actual == expected_template);
 
         let raw_with_brackets = format!("[&{}]", raw);
         let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
-        assert_matches!(result_with_brackets, Ok(_));
+
+        assert_matches!(result_with_brackets, Ok(ChatLink::WardrobeTemplate(actual)) if actual == expected_template);
     }
 
     #[test]
     fn test_parse_zizi() {
         let raw = ZIZI_TEMPLATE;
-        
         let result = ChatLink::try_from(raw);
-        assert_matches!(result, Ok(_));
 
-        let raw_with_brackets = format!("[&{}]", raw);
-        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
-        assert_matches!(result_with_brackets, Ok(_));
+        let Ok(ChatLink::WardrobeTemplate(actual)) = result else {
+           panic!("Expected WardrobeTemplate, got {result:?}");
+        };
+
+        for slot in actual {
+            match slot {
+                EquipmentSlot::Skin(skin) => {
+                    match skin.skin_type {
+                        SkinType::WeaponB2 => {
+                            assert_eq!(skin.skin, 0.into(), "Expected unset skin_id for {skin:?}");
+                            assert!(skin.visible, "Expected skin to be visible for {skin:?}");
+                        },
+                        SkinType::Aquabreather => {
+                            assert_ne!(skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(!skin.visible, "Expected skin to not be visible for {skin:?}");
+                        },
+                        SkinType::WeaponAquaticA | SkinType::WeaponAquaticB | SkinType::WeaponA1 | SkinType::WeaponA2 | SkinType::WeaponB1 => {
+                            assert_ne!(skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(skin.visible, "Expected skin to be visible for {skin:?}");
+                        },
+                        _ => panic!("Dyable skin should not be non-dyable {skin:?}")
+                    }
+                }
+                EquipmentSlot::DyableSkin(skin) => {
+                    match skin.skin.skin_type {
+                        SkinType::Outfit => {
+                            assert_ne!(skin.skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(!skin.skin.visible, "Expected skin to not be visible for {skin:?}");
+                            assert_eq!(skin.dyes, (1, 1, 1, 1).into(), "Expected unset dyes for {skin:?}");
+                        },
+                        SkinType::Backpack => {
+                            assert_ne!(skin.skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(skin.skin.visible, "Expected skin to be visible for {skin:?}");
+                            assert_eq!(skin.dyes, (1, 1, 1, 1).into(), "Expected unset dyes for {skin:?}");
+                        },
+                        SkinType::Chest | SkinType::Shoes | SkinType::Gloves | SkinType::Head | SkinType::Legs | SkinType::Shoulders => {
+                            assert_ne!(skin.skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(skin.skin.visible, "Expected skin to be visible for {skin:?}");
+                            assert_ne!(skin.dyes, (1, 1, 1, 1).into(), "Expected dyes set for {skin:?}");
+                        },
+                        _ => panic!("Skin should not be dyable {skin:?}")
+                    }
+                }
+            }
+        }
     }
 
     #[test]
     fn test_parse_zizi_armor_only() {
         let raw = ZIZI_ARMOR_TEMPLATE;
-        
         let result = ChatLink::try_from(raw);
-        assert_matches!(result, Ok(_));
 
-        let raw_with_brackets = format!("[&{}]", raw);
-        let result_with_brackets = ChatLink::try_from(raw_with_brackets.as_str());
-        assert_matches!(result_with_brackets, Ok(_));
+        let Ok(ChatLink::WardrobeTemplate(actual)) = result else {
+           panic!("Expected WardrobeTemplate, got {result:?}");
+        };
+
+        for slot in actual {
+            match slot {
+                EquipmentSlot::Skin(skin) => {
+                    match skin.skin_type {
+                        SkinType::WeaponAquaticA | SkinType::WeaponAquaticB | SkinType::WeaponA1 | SkinType::WeaponA2 | SkinType::WeaponB1 | SkinType::WeaponB2 | SkinType::Aquabreather => {
+                            assert_eq!(skin.skin, 0.into(), "Expected unset skin_id for {skin:?}");
+                            assert!(skin.visible, "Expected skin to be visible for {skin:?}");
+                        },
+                        _ => panic!("Dyable skin should not be non-dyable {skin:?}")
+                    }
+                }
+                EquipmentSlot::DyableSkin(skin) => {
+                    match skin.skin.skin_type {
+                        SkinType::Outfit => {
+                            assert_eq!(skin.skin.skin, 0.into(), "Expected unset skin_id for {skin:?}");
+                            assert!(skin.skin.visible, "Expected skin to be visible for {skin:?}");
+                            assert_eq!(skin.dyes, (1, 1, 1, 1).into(), "Expected unset dyes for {skin:?}");
+                        },
+                        SkinType::Backpack => {
+                            assert_ne!(skin.skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(skin.skin.visible, "Expected skin to be visible for {skin:?}");
+                            assert_eq!(skin.dyes, (1, 1, 1, 1).into(), "Expected unset dyes for {skin:?}");
+                        },
+                        SkinType::Chest | SkinType::Shoes | SkinType::Gloves | SkinType::Head | SkinType::Legs | SkinType::Shoulders => {
+                            assert_ne!(skin.skin.skin, 0.into(), "Expected skin_id set for {skin:?}");
+                            assert!(skin.skin.visible, "Expected skin to be visible for {skin:?}");
+                            assert_ne!(skin.dyes, (1, 1, 1, 1).into(), "Expected dyes set for {skin:?}");
+                        },
+                        _ => panic!("Skin should not be dyable {skin:?}")
+                    }
+                }
+            }
+        }
+    }
+
+    fn empty_skin(skin_type: SkinType) -> EquipmentSlot {
+        let skin = Skin::new(skin_type, 0.into(), true);
+        if skin_type.dyable() {
+            DyableSkin::new(skin, (1, 1, 1, 1).into()).into()
+        } else {
+            skin.into()
+        }
     }
 }
