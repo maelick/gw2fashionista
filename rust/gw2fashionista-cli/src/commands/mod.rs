b92f8010b563f29c12bf5ecedbbda664bd5b2489
Maëlick Claes <himself@maelick.net>
2026-06-05 12:21:32 +0000

    CLI: refactor wardrobe templates as subsubcommands

--- rust/gw2fashionista-cli/src/commands/mod.rs
+++ rust/gw2fashionista-cli/src/commands/mod.rs
@@ -1,30 +1,44 @@
-use clap::{Subcommand};
+use clap::{Subcommand, Args};
 
 mod args;
-pub mod export;
-pub mod read;
-pub mod merge;
-pub mod filter;
+mod read;
+mod wardrobe_export;
+mod wardrobe_merge;
+mod wardrobe_filter;
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 pub enum Commands {
-    /// Export equipment tabs as fashion templates using the API.
-    Export(export::Command),
-    /// Read a fashion template and prints its content by retrieving values from the GW2 API.
+    /// Read a chat linkand prints its content, potentially resolving ids by retrieving values from the GW2 API.
     Read(read::Command),
-    /// Merge two fashion templates by overriding specific parts of the first one with values of the second one
-    Merge(merge::Command),
-    /// Filter a fashion template to include only specific parts.
-    Filter(filter::Command),
+    /// Wardrobe template commands
+    Wardrobe(WardrobeArgs)
+}
+
+#[derive(Args, Debug)]
+pub struct WardrobeArgs {
+    #[command(subcommand)]
+    command: WardrobeCommands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum WardrobeCommands {
+    /// Export equipment tabs as wardrobe templates using the API.
+    Export(wardrobe_export::Command),
+    /// Merge two wardrobe templates by overriding specific parts of the first one with values of the second one
+    Merge(wardrobe_merge::Command),
+    /// Filter a wardrobe template to include only specific parts.
+    Filter(wardrobe_filter::Command),
 }
 
 impl Commands {
     pub fn as_command(&self) -> &dyn Command {
         match self {
-            Commands::Export(cmd) => cmd,
             Commands::Read(cmd) => cmd,
-            Commands::Merge(cmd) => cmd,
-            Commands::Filter(cmd) => cmd,
+            Commands::Wardrobe(args) => match &args.command {
+                WardrobeCommands::Export(cmd) => cmd,
+                WardrobeCommands::Merge(cmd) => cmd,
+                WardrobeCommands::Filter(cmd) => cmd,
+            }
         }
     }
 }
