742a8a1bd62a6e713236579d3ebd9427c7007bda
Maëlick Claes <himself@maelick.net>
2026-06-18 12:39:38 +0000

    implement reading from CSV

--- rust/gw2fashionista-cli/src/commands/read.rs
+++ rust/gw2fashionista-cli/src/commands/read.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, iter};
 use clap::{Args};
 use gw2fashionista_core::models::wardrobe_template::WardrobeTemplateData;
 use gw2fashionista_core::domain::{chatlink::ChatLink, error::ChatLinkError, wardrobe_template::WardrobeTemplate};
@@ -13,20 +13,51 @@ pub struct Command {
 impl Command {
     fn get_links(&self) -> anyhow::Result<Vec<String>> {
         if self.chat_links.is_empty() {
-            Ok(self.read_links(io::stdin().lock())?)
+            self.read_links(io::stdin().lock())
         } else {
             Ok(self.chat_links.clone())
         }
     }
 
-    fn read_links<R: io::BufRead>(&self, reader: R) -> io::Result<Vec<String>> {
-        Ok(reader
-            .lines()
-            .collect::<io::Result<Vec<_>>>()?
-            .into_iter()
-            .filter(|l| !l.is_empty())
-            .collect()
-        )
+    fn read_links<R: io::BufRead>(&self, reader: R) -> anyhow::Result<Vec<String>> {
+        let mut reader = csv::Reader::from_reader(reader);
+        let headers = reader.headers()?.clone();
+        if headers.len() == 0 {
+            Err(anyhow::anyhow!("Empty CSV input"))
+        } else if headers.len() == 1 {
+            self.read_single_column(headers, &mut reader)
+        } else {
+            self.read_multiple_columns(headers, &mut reader)
+        }
+    }
+
+    fn read_single_column<R: io::BufRead>(&self, headers: csv::StringRecord, reader: &mut csv::Reader<R>) -> anyhow::Result<Vec<String>> {
+        let first = iter::once(Ok(headers.get(0).unwrap().to_string()));
+        let others = self.read_from_column(reader, 0);
+        first.chain(others).collect()
+    }
+
+    fn read_multiple_columns<R: io::BufRead>(&self, headers: csv::StringRecord, reader: &mut csv::Reader<R>) -> anyhow::Result<Vec<String>> {
+        let col = self.find_column(headers, "fashion_link")?;
+        self.read_from_column(reader, col).collect()
+    }
+
+    fn read_from_column<R: io::BufRead>(&self, reader: &mut csv::Reader<R>, col: usize) -> impl Iterator<Item = anyhow::Result<String>> {
+        reader.records().map(move |r| self.read_chat_link(r?, col))
+    }
+
+    fn read_chat_link(&self, record: csv::StringRecord, col: usize) -> anyhow::Result<String> {
+        match record.get(col) {
+            Some(value) => Ok(value.to_string()),
+            None => Err(anyhow::anyhow!("Invalid CSV file: row {} missing column {}", 0, col)),
+        }
+    }
+
+    fn find_column(&self, headers: csv::StringRecord, col_name: &str) -> anyhow::Result<usize> {
+        match headers.iter().position(|header| header == col_name) {
+            Some(value) => Ok(value),
+            None => Err(anyhow::anyhow!("Missing column {} in CSV input", col_name))
+        }
     }
 }
 
