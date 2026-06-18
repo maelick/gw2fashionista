29fa2b94ca8cbdf9bd3c91d2ed61d1e9ebb0ad9f
Maëlick Claes <himself@maelick.net>
2026-06-18 11:18:05 +0000

    export CLI output to file as CSV or JSON

--- rust/gw2fashionista-cli/src/commands/wardrobe_export.rs
+++ rust/gw2fashionista-cli/src/commands/wardrobe_export.rs
@@ -1,5 +1,7 @@
+use std::{io, fs};
 use clap::{Args};
-use gw2fashionista_core::{domain::chatlink::ChatLink, gw2_data::{Resolver, import::Importer}};
+use serde::{Deserialize, Serialize};
+use gw2fashionista_core::{domain::{chatlink::ChatLink, error::ChatLinkError}, gw2_data::{Resolver, equipment::Equipment, import::Importer}};
 
 use super::args;
 
@@ -40,12 +42,82 @@ impl super::Command for Command {
         let equipments = importer.fetch_equipment(&self.characters)?;
 
         let mut resolver = Resolver::default();
-        let equipments = resolver.resolve_equipment(equipments)?;
+        let equipments: Result<Vec<_>, _> = resolver
+            .resolve_equipment(equipments)?
+            .iter()
+            .map(ExportedEquipment::new)
+            .collect();
 
-        for e in &equipments {
-            let chatlink = ChatLink::WardrobeTemplate(e.into());
-            println!("{}", chatlink.to_string()?);
+        self.output_equipments(equipments?)?;
+        Ok(())
+    }
+}
+
+impl Command {
+    fn output_equipments(&self, equipments: Vec<ExportedEquipment>) -> anyhow::Result<()> {
+        let format = match self.format {
+            args::Format::Auto => self.detect_format(),
+            _ => self.format,
+        };
+        Ok(match format {
+            args::Format::CSV => self.output_csv(equipments)?,
+            args::Format::JSON => self.output_json(equipments)?,
+            _ => todo!(),
+        })
+    }
+
+    fn detect_format(&self) -> args::Format {
+        match &self.output {
+            Some(path) => match path.extension() {
+                Some(ext) if ext == "json" => args::Format::JSON,
+                _ => args::Format::CSV
+            },
+            None => args::Format::CSV,
+        }
+    }
+
+    fn open_output(&self) -> anyhow::Result<Box<dyn io::Write>> {
+        self.output
+            .as_ref()
+            .map(open_file)
+            .unwrap_or_else(|| Ok(Box::new(io::stdout())))
+    }
+
+    fn output_csv(&self, equipments: Vec<ExportedEquipment>) -> anyhow::Result<()> {
+        let mut writer = csv::Writer::from_writer(self.open_output()?);
+        for e in equipments {
+            writer.serialize(e)?;
         }
         Ok(())
     }
+
+    fn output_json(&self, equipments: Vec<ExportedEquipment>) -> anyhow::Result<()> {
+        serde_json::to_writer_pretty(self.open_output()?, &equipments)?;
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct ExportedEquipment {
+    pub char_name: String,
+    pub tab_id: usize,
+    pub tab_name: String,
+    pub fashion_link: String,
+}
+
+impl ExportedEquipment {
+    pub fn new(equipment: &Equipment) -> Result<Self, ChatLinkError> {
+        let chat_link = ChatLink::WardrobeTemplate(equipment.into());
+        Ok(ExportedEquipment{
+            char_name: equipment.char_name.clone(),
+            tab_id: equipment.tab_id,
+            tab_name: equipment.tab_name.clone(),
+            fashion_link: chat_link.to_string()?,
+        })
+    }
+}
+
+fn open_file(path: &std::path::PathBuf) -> anyhow::Result<Box<dyn io::Write>> {
+    let writer = fs::OpenOptions::new().write(true).create(true).open(path)?;
+    Ok(Box::new(writer))
 }
