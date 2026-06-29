082d1e42a4e7f1bab4ed08eb7077c12a5b7dfcb5
Maëlick Claes <himself@maelick.net>
2026-06-29 09:47:28 +0000

    CLI e2e tests: use insta + rstest + write tests for both read with and without --pretty

--- e2e/src/cli.rs
+++ e2e/src/cli.rs
@@ -1,6 +1,7 @@
 use assert_cmd::Command;
+use rstest::rstest;
 
-use gw2fashionista_fixtures::wardrobe::{EMPTY_TEMPLATE};
+use gw2fashionista_fixtures::wardrobe::{WardrobeTemplate, EMPTY_TEMPLATE, PEEKABOO_TEMPLATE, ZIZI_ARMOR_TEMPLATE, ZIZI_TEMPLATE};
 
 fn spawn_cli(args: &[&str]) -> std::process::Output {
     Command::cargo_bin("gw2fashionista-cli")
@@ -10,8 +11,26 @@ fn spawn_cli(args: &[&str]) -> std::process::Output {
         .expect("Failed to run command")
 }
 
-#[test]
-fn test_read_command() {
-    let output = spawn_cli(&["read", EMPTY_TEMPLATE.chat_link]);
+#[rstest]
+#[case(EMPTY_TEMPLATE)]
+#[case(PEEKABOO_TEMPLATE)]
+#[case(ZIZI_TEMPLATE)]
+#[case(ZIZI_ARMOR_TEMPLATE)]
+fn test_read_command(#[case] template: WardrobeTemplate) {
+    let output = spawn_cli(&["read", template.chat_link]);
     assert!(output.status.success());
+    let json: serde_json::Value = serde_json::from_slice(&output.stdout).unwrap();
+    insta::assert_json_snapshot!(template.snapshot_name("read"), json);
+}
+
+#[rstest]
+#[case(EMPTY_TEMPLATE)]
+#[case(PEEKABOO_TEMPLATE)]
+#[case(ZIZI_TEMPLATE)]
+#[case(ZIZI_ARMOR_TEMPLATE)]
+fn test_read_command_pretty(#[case] template: WardrobeTemplate) {
+    let output = spawn_cli(&["read", template.chat_link, "--pretty"]);
+    assert!(output.status.success());
+    let json: serde_json::Value = serde_json::from_slice(&output.stdout).unwrap();
+    insta::assert_json_snapshot!(template.snapshot_name("read"), json);
 }
