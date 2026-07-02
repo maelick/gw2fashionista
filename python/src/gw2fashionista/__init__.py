5af003d7c87270762bd18aa5a064164f5cdf78d9
Maëlick Claes <himself@maelick.net>
2026-07-02 17:32:11 +0000

    remove python folder

--- python/src/gw2fashionista/__init__.py
+++
@@ -1,5 +0,0 @@
-from gw2fashionista.template import FashionTemplate
-from gw2fashionista.chatlink import ChatLink
-from gw2fashionista.skins import Skin, DyableSkin
-from gw2fashionista.enums.skin import SkinType, SkinFlag
-from gw2fashionista.cache import Cache
--- python/src/gw2fashionista/__main__.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-from gw2fashionista.cli import CLI
-
-if __name__ == '__main__':
-    load_dotenv()
-    _ = CLI().run()
