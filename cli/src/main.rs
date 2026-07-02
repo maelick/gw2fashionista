4f156a65fcb89ad58cc9334f1fbfd77293acb60d
Maëlick Claes <himself@maelick.net>
2026-07-02 11:39:05 +0000

    CLI: use tracing for logging + add global options to subscribe to all logs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,13 +1,21 @@
+use std::io::{self, IsTerminal};
+
 use clap::{Parser};
 use clap_verbosity_flag::{Verbosity, InfoLevel};
 
 use gw2fashionista_cli::commands::{Commands};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Parser)]
 #[command(version, about = "GW2 Fashion Exporter CLI", long_about = None)]
 struct Cli {
     #[command(flatten)]
-    verbosity: Verbosity<InfoLevel>, // TODO: change to warn instead?
+    verbosity: Verbosity<InfoLevel>,
+
+    /// Output all logs (from external libraries)
+    #[arg(long, global = true)]
+    pub all_logs: bool,
 
     #[command(subcommand)]
     command: Commands,
@@ -15,23 +23,39 @@ struct Cli {
 
 impl Cli {
     fn init(&self) {
-        env_logger::Builder::new()
-            .filter_level(self.verbosity.into())
-            .init();
+        self.init_tracing(self.verbosity.into(), self.all_logs);
+    }
+
+    fn init_tracing(&self, level: LevelFilter, all_logs: bool) {
+        let layer = tracing_subscriber::fmt::layer()
+            .with_writer(tracing_writer)
+            .with_filter(level)
+            .with_filter(tracing_subscriber::filter::filter_fn(move |metadata| {
+                all_logs || metadata.target().starts_with("gw2fashionista")
+            }));
+        tracing_subscriber::registry().with(layer).init()
     }
 
     async fn execute(&self) {
         let cmd = self.command.as_command();
-        log::debug!("Executing command {}: {:?}", cmd.name(), cmd);
+        tracing::debug!("Executing command {}: {:?}", cmd.name(), cmd);
         match cmd.execute().await {
             Ok(_) => {
-                log::debug!("Command {} successful", cmd.name())
+                tracing::debug!("Command {} successful", cmd.name())
             },
             Err(err) => {
-                log::error!("Command {} error: {}", cmd.name(), err);
+                tracing::error!("Command {} error: {}", cmd.name(), err);
                 std::process::exit(1)
             }
         }
+    }
+}
+
+fn tracing_writer() -> Box<dyn io::Write> {
+    if io::stdout().is_terminal() {
+        Box::new(io::stdout())
+    } else {
+        Box::new(io::stderr())
     }
 }
 
