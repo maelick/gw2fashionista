d9e5327bdf229d7f25201fab524709ed724d404e
Maëlick Claes <himself@maelick.net>
2026-07-13 16:53:00 +0000

    refactor: fixtures: rename test cases to include wardrobe/travel

--- fixtures/src/travel.rs
+++ fixtures/src/travel.rs
@@ -10,30 +10,30 @@ pub const ALL_TEMPLATES: &[TravelTemplate] = &[
 
 /// Travel template with nothing set
 pub const EMPTY_TEMPLATE: TravelTemplate = TravelTemplate::new(
-    "empty",
+    "travel_empty",
     "EAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAP8P",
 );
 
 /// Travel template with default skins with shadow abyss dye
 pub const DEFAULT_MOUNT_TEMPLATE: TravelTemplate = TravelTemplate::new(
-    "default",
+    "travel_default",
     "EAIASgUBAAEAAQACAAEAAQABAAEABgABAAEAAQBKBQQASgUBAAEAAQADAEoFAQABAAEAAgBKBQEAAQABAAEASgUBAAEAAQBnAAEAAQABAEoFoQBKBQEAAQABALoASgUBAAEAAQCaAUoFAQABAAEAjQFKBQEAAQABAP8P",
 );
 
 /// Travel template with some skins set (except turtle) with no dye set
 pub const NO_DYES_TEMPLATE: TravelTemplate = TravelTemplate::new(
-    "no_dyes",
+    "travel_no_dyes",
     "EJkAygHbAV0GOwYFAD0GgQCeAoECYQC7AXsAhgS6AvYATQAwBkwCbgKnAVcAGQVHBWQGhwHcAS4GPQY7BkkBUAEEACgGoQIlARkAaQYuBl0GwgKtBlMAYwIOBowBUQDAAUAACQDDAVUATAF5AZwC+AGZAlMCbQCIAv8P",
 );
 
 /// Peekabo's travel template
 pub const PEEKABOO_TEMPLATE: TravelTemplate = TravelTemplate::new(
-    "peekaboo",
+    "travel_peekaboo",
     "EJAABgDiBOIEHgUFAB4F4gTiBOIEbgIeBeIEHgXiBEoBBgAeBUgF4gTzAeIEHgVIBR4FJAPiBOIEHgVIBZMBBgAeBeIESAXnAkgFHgXiBOIEyAIGAB4F4gRIBXAC4gQGAAYAXwHDAR4F4gQeBeIE+AEeBeIESAUeBf8P",
 );
 
 /// Zizï Skyhoof's travel template
 pub const ZIZI_TEMPLATE: TravelTemplate = TravelTemplate::new(
-    "zizi",
+    "travel_zizi",
     "EJkAGAUYBREGEQYFAH4AgwAYBYECbgK7ARgFGAUYBUoBEQYYBRgFGAXzARgFEQYYBRgFJAMRBhgFfgB+AJMBEQYYBagCDQbnAhgFEQYYBdkByAIRBhgFqAJ+AHACGAURBhEGGAWdAREGGAUYBREG+AERBhgFGAURBv8P",
 );
--- fixtures/src/wardrobe.rs
+++ fixtures/src/wardrobe.rs
@@ -9,24 +9,24 @@ pub const ALL_TEMPLATES: &[WardrobeTemplate] = &[
 
 /// Wardrobe template with nothing set
 pub const EMPTY_TEMPLATE: WardrobeTemplate = WardrobeTemplate::new(
-    "empty",
+    "wardrobe_empty",
     "DwAAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAEAAQABAAEAAAABAAEAAQABAAAAAQABAAEAAQAAAAAAAAAAAAAAAAD/fw==",
 );
 
 /// Peekaboo's wardrobe template
 pub const PEEKABOO_TEMPLATE: WardrobeTemplate = WardrobeTemplate::new(
-    "peekaboo",
+    "wardrobe_peekaboo",
     "D7UfzTMeBQYA4gQGAJ4AHgUGAB4FAQCsAAYABgAeBQEANSgBAAYAHgUBAMkDHgUGAAEAAQDVAAYAHgUeBQEAoRYeBQYAAQABADIAAQABAAEAAQBoEqAPFCovKj8SAAD/fg==",
 );
 
 /// Zizï Skyhoof's wardrobe template
 pub const ZIZI_TEMPLATE: WardrobeTemplate = WardrobeTemplate::new(
-    "zizi",
+    "wardrobe_zizi",
     "D1sDPQkBAAEAAQABAAwAGAURBhEGAQAjABgFEQYBAAEA/AABABEGGAUYBdIDGAURBgEAAQALAAEAEQYRBgEAohYYBREGAQABAHwAAQABAAEAAQDjE6APPBI8Ej0SAAD+fg==",
 );
 
 /// Zizï Skyhoof's armor (+ backpack) template
 pub const ZIZI_ARMOR_TEMPLATE: WardrobeTemplate = WardrobeTemplate::new(
-    "zizi_armor",
+    "wardrobe_zizi_armor",
     "DwAAPQkBAAEAAQABAAwAGAURBhEGAQAjABgFEQYBAAEA/AABABEGGAUYBdIDGAURBgEAAQALAAEAEQYRBgEAohYYBREGAQABAAAAAQABAAEAAQAAAAAAAAAAAAAAAAD/fw==",
 );
