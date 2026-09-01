7b01e4e44c14d6f8f3db6554b469e0492ab03115
Maëlick Claes <himself@maelick.net>
2026-09-01 08:26:32 +0000

    refactor: CLI: move subcommands in their respective module

--- cli/src/commands/travel/mod.rs
+++ cli/src/commands/travel/mod.rs
@@ -1,3 +1,28 @@
+use crate::commands::Command;
+
 pub mod args;
 pub mod filter;
 pub mod merge;
+
+#[derive(clap::Args, Debug)]
+pub struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+impl Args {
+    pub(crate) fn command(&self) -> &dyn Command {
+        match &self.command {
+            Commands::Merge(cmd) => cmd,
+            Commands::Filter(cmd) => cmd,
+        }
+    }
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub enum Commands {
+    /// Merge two travel templates by overriding specific parts of the first one with values of the second one
+    Merge(merge::Command),
+    /// Filter a travel template to include only specific parts.
+    Filter(filter::Command),
+}
