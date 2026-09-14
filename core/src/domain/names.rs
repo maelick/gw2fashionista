279ba1f3a7914f392b72f4f484c351b0bcea3463
Maëlick Claes <himself@maelick.net>
2026-09-14 21:34:17 +0000

    feat: normalize fashion, character and tag names

--- core/src/domain/names.rs
+++ core/src/domain/names.rs
@@ -1,4 +1,7 @@
+use std::{borrow::Cow, sync::LazyLock};
+
 use nutype::nutype;
+use regex::Regex;
 
 #[nutype(
     sanitize(trim, with = normalize_fashion_name),
@@ -8,7 +11,7 @@ use nutype::nutype;
 pub struct FashionName(String);
 
 fn normalize_fashion_name(s: String) -> String {
-    s
+    normalize_whitespaces(&s).to_string()
 }
 
 #[nutype(
@@ -19,7 +22,23 @@ fn normalize_fashion_name(s: String) -> String {
 pub struct CharacterName(String);
 
 fn normalize_char_name(s: String) -> String {
-    s
+    capitalize_character_name(normalize_whitespaces(&s).as_ref())
+}
+
+fn capitalize_character_name(s: &str) -> String {
+    let words: Vec<_> = s.split_whitespace().map(capitalize_word).collect();
+    words.join(" ")
+}
+
+fn capitalize_word(word: &str) -> String {
+    let mut chars = word.chars();
+    match chars.next() {
+        None => String::default(),
+        Some(first) => {
+            let first = first.to_string().to_uppercase();
+            first + &chars.as_str().to_lowercase()
+        }
+    }
 }
 
 #[nutype(
@@ -30,5 +49,11 @@ fn normalize_char_name(s: String) -> String {
 pub struct TagName(String);
 
 fn normalize_tag_name(s: String) -> String {
-    s
+    normalize_whitespaces(&s).to_string()
+}
+
+static RE_WHITESPACES: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\s+").unwrap());
+
+fn normalize_whitespaces<'a>(s: &'a str) -> Cow<'a, str> {
+    RE_WHITESPACES.replace_all(s, " ")
 }
